614a1cc6c0a0029a1e4da5384acda5b2ba124974
A-Rejo <10016283@sbstudents.org>
2018-12-05 08:05:43 -0500

    Default and Non-Default Constructors
    Created Default and Non-Default Constructors to assign the value of Sides and Value of the 6-Sided die and the user created die.

--- Die.java
+++ Die.java
@@ -5,5 +5,13 @@ public class Die{
 	private int Sides;
 	private int Value;
 
+	public Die(){
+		Sides = 6;
+		Value = (int)(Math.random()*Sides)+1;
+	}
+	public Die(int Sides){
+		this.Sides = Sides;
+		Value = (int)(Math.random()*Sides)+1;
+	}
 
 }
