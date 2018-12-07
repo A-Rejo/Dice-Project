623e9a821ae119f734fa769f830cc3145532a7ec
ZeyadE <45392084+ZeyadE@users.noreply.github.com>
2018-12-07 07:50:17 -0500

    Created DiceHolder class
    Able to hold 6 dice, add new ones, and shake the existing ones.

--- DiceHolder.java
+++ DiceHolder.java
@@ -1,8 +1,27 @@
+import java.util.*;
 public class DiceHolder{
+	private ArrayList<Die> dice;
+		public DiceHolder(){
+		dice = new ArrayList<Die>();
+	}
+	public void Shake(){
+		Die temp;
+		for(int i = 0; i < dice.size(); i++){
+			int index = (int)
+		(Math.random()*dice.size()-1)+0;
+		temp = dice.get(index);
+		dice.set(i, dice.get(index));
+		dice.set(index, temp);
+	}
+}
+	public int addDie(Die x){
+		if(dice.size()<6){
+			dice.add(x);
+			return 1;
+		}
+		return -1;
+	}
 
-	private holder;
-
-	public int addDie
 
 
 
