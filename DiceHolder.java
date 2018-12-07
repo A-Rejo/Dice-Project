284072ddfe072108e6d443ed59e4e3827fc8da6f
ZeyadE <45392084+ZeyadE@users.noreply.github.com>
2018-12-07 08:12:13 -0500

    Added toString method
    prints out all the die in the holder in reader friendly language.

--- DiceHolder.java
+++ DiceHolder.java
@@ -22,7 +22,16 @@ public class DiceHolder{
 		return -1;
 	}
 
-
+	public String toString(){
+		if (dice.size() == 0){
+			System.out.println("There are no dice in this holder, try adding a die.");
+	}
+		String st = ("");
+			for(int i = 0; i < dice.size(); i++){
+			st += dice.get(i)+"\n";
+		}
+		return st;
+	}
 
 
 }
